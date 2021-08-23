876adf4cd7f22db225f33d7ec245c2d44a680d9a
Rongxin Liu <rongxinliu.dev@gmail.com>
2021-08-23 01:32:02 -0400

    refactored extension codes

--- src/extension.ts
+++ src/extension.ts
@@ -6,57 +6,51 @@ const PYTHON = "Python";
 const TRIGGER_PYTHON_DEBUG = ".vscode/debug50/triggers/python";
 const TRIGGER_CPP_DEBUG = ".vscode/debug50/triggers/c";
 const SOCKET = ".vscode/debug50/socket";
-
-let wf = vscode.workspace.workspaceFolders[0].uri.path;
-
-function init() {
-	vscode.workspace.fs.writeFile(vscode.Uri.file(`${wf}/${TRIGGER_PYTHON_DEBUG}`), new TextEncoder().encode());
-	vscode.workspace.fs.writeFile(vscode.Uri.file(`${wf}/${TRIGGER_CPP_DEBUG}`), new TextEncoder().encode());
-	vscode.workspace.fs.writeFile(vscode.Uri.file(`${wf}/${SOCKET}`), new TextEncoder().encode());
-}
-
-function checkBreakPoints() {
-	if (vscode.debug.breakpoints.length === 0) {
-		relay("no_break_points");
-		return false;
-	}
-	return true;
-}
-
-function relay(message: string) {
-	var uint8array = new TextEncoder().encode(message);
-	vscode.workspace.fs.writeFile(vscode.Uri.file(`${wf}/${SOCKET}`), uint8array);
-}
+const WORKSPACE_FOLDER = vscode.workspace.workspaceFolders[0];
+const WORKSPACE_FOLDER_PATH = WORKSPACE_FOLDER.uri.path;
 
 export async function activate(context: vscode.ExtensionContext) {
 	init();
 
 	// Setup fileWatcher
-	let folders = vscode.workspace.workspaceFolders;
-	if (folders) {
-		let initPythonDebug = vscode.workspace.createFileSystemWatcher(
-			new vscode.RelativePattern(folders[0], TRIGGER_PYTHON_DEBUG));
-		initPythonDebug.onDidChange((uri) => {
-			if (folders !== undefined && checkBreakPoints()) {
-				vscode.debug.startDebugging(folders[0], PYTHON);
-			}
-		});
-
-		let initCPPDebug = vscode.workspace.createFileSystemWatcher(
-			new vscode.RelativePattern(folders[0], TRIGGER_CPP_DEBUG));
-		initCPPDebug.onDidChange((uri) => {
-			if (folders !== undefined && checkBreakPoints()) {
-				vscode.debug.startDebugging(folders[0], CPP);
-			}
-		});
-	}
-
-	vscode.debug.onDidTerminateDebugSession((event) => {
-		relay("debug_session_terminated");
+	vscode.workspace.createFileSystemWatcher(
+		new vscode.RelativePattern(WORKSPACE_FOLDER, TRIGGER_PYTHON_DEBUG)
+	).onDidChange((uri) => {
+		if (checkBreakPoints()) {
+			vscode.debug.startDebugging(WORKSPACE_FOLDER, PYTHON);
+		}
 	});
 
-	relay("debug50_activated");
+	vscode.workspace.createFileSystemWatcher(
+		new vscode.RelativePattern(WORKSPACE_FOLDER, TRIGGER_CPP_DEBUG)
+	).onDidChange((uri) => {
+		if (checkBreakPoints()) {
+			vscode.debug.startDebugging(WORKSPACE_FOLDER, CPP);
+		}
+	});
+
+	vscode.debug.onDidTerminateDebugSession((event) => {
+		notify("debug_session_terminated");
+	});
+
+	notify("debug50_activated");
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+function init() {
+	vscode.workspace.fs.writeFile(vscode.Uri.file(`${WORKSPACE_FOLDER_PATH}/${TRIGGER_PYTHON_DEBUG}`), new TextEncoder().encode());
+	vscode.workspace.fs.writeFile(vscode.Uri.file(`${WORKSPACE_FOLDER_PATH}/${TRIGGER_CPP_DEBUG}`), new TextEncoder().encode());
+	vscode.workspace.fs.writeFile(vscode.Uri.file(`${WORKSPACE_FOLDER_PATH}/${SOCKET}`), new TextEncoder().encode());
+}
+
+function checkBreakPoints() {
+	if (vscode.debug.breakpoints.length === 0) {
+		notify("no_break_points");
+		return false;
+	}
+	return true;
+}
+
+function notify(message: string) {
+	vscode.workspace.fs.writeFile(
+		vscode.Uri.file(`${WORKSPACE_FOLDER_PATH}/${SOCKET}`), new TextEncoder().encode(message));
+}
