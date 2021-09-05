86b348c37320e134f580f817e15785e11c4f14fa
Rongxin Liu <rongxinliu.dev@gmail.com>
2021-09-05 18:57:02 -0400

    made debug50 extension into a separate module

--- src/extension.ts
+++ src/extension.ts
@@ -1,49 +1,12 @@
 import * as vscode from 'vscode';
-import WebSocket = require('ws');
+import * as debug from './debug';
 
-const WORKSPACE_FOLDER = vscode.workspace.workspaceFolders[0];
 const PORT = 60001;
-const LAUNCH_CONFIG_C = "C";
-const LAUNCH_CONFIG_PYTHON = "Python";
-
-let wss: WebSocket.Server | null = null;
-let ws: WebSocket | null = null;
-
-const startWebsocketServer = async (port: number, fallbackPorts: number[]): Promise<void> => {
-
-	wss = new WebSocket.Server({ port });
-
-	wss.on('connection', (connection: any) => {
-		ws = connection;
-		if (ws) {
-			ws.addEventListener('message', (event) => {
-				if (event.data == "check_break_points") {
-					ws.send(vscode.debug.breakpoints.length);
-				}
-				if (event.data == "start_c_debug") {
-					vscode.debug.startDebugging(WORKSPACE_FOLDER, LAUNCH_CONFIG_C);
-				}
-				if (event.data == "start_python_debug") {
-					vscode.debug.startDebugging(WORKSPACE_FOLDER, LAUNCH_CONFIG_PYTHON);
-				}
-			});
-		}
-	});
-
-};
 
 export function activate({ subscriptions }: vscode.ExtensionContext) {
-	startWebsocketServer(PORT, [PORT + 1]);
-	vscode.debug.onDidStartDebugSession((event) => {
-		ws.send("started_debugger");
-	});
-	vscode.debug.onDidTerminateDebugSession((event) => {
-		ws.send("terminated_debugger");
-	});
+	debug.startWebsocketServer(PORT, [PORT + 1]);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
-	ws?.close();
-	wss?.close();
+	debug.stopWebsocketServer();
 }
