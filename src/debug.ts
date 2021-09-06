85c78977e9a4902104d1520891437854ee73474f
Rongxin Liu <rongxinliu.dev@gmail.com>
2021-09-06 14:44:41 -0400

    dynamically generate debugger launch configuration

--- src/debug.ts
+++ src/debug.ts
@@ -1,9 +1,17 @@
 import * as vscode from 'vscode';
 import WebSocket = require('ws');
 
+// Initialization
+vscode.debug.breakpoints
+
 interface payload {
 	"command": string,
 	"path": string
+	"config": {
+		"name": string,
+		"type": string,
+		"request": string
+	}
 }
 
 interface breakpoint {
@@ -17,24 +25,20 @@ interface breakpoint {
 	}
 }
 
-const WORKSPACE_FOLDER = vscode.workspace.workspaceFolders[0];
-const LAUNCH_CONFIG_C = "C";
-const LAUNCH_CONFIG_PYTHON = "Python";
-
 let wss: WebSocket.Server | null = null;
 let ws: WebSocket | null = null;
 
-// Initialization
-vscode.debug.breakpoints
+const WORKSPACE_FOLDER = vscode.workspace.workspaceFolders[0];
 
-const startDebugger = (workspace_folder, launch_config, path) => {
+const startDebugger = (workspace_folder, payload: payload) => {
 	let didSetBreakpoints = false;
 	let breakpoints = vscode.debug.breakpoints;
 	for (var each in vscode.debug.breakpoints) {
 		let breakpoint: breakpoint = JSON.parse(JSON.stringify(breakpoints[each]));
-		if (breakpoint.location.uri.path === path) {
+		if (breakpoint.location.uri.path === payload.path) {
 			didSetBreakpoints = true;
-			vscode.debug.startDebugging(workspace_folder, launch_config)
+			let config:vscode.DebugConfiguration = payload.config
+			vscode.debug.startDebugging(workspace_folder, config)
 		}
 	}
 	if (!didSetBreakpoints) {
@@ -49,12 +53,7 @@ const startWebsocketServer = async (port: number, fallbackPorts: number[]): Prom
 		if (ws) {
 			ws.addEventListener('message', (event) => {
 				let payload: payload = JSON.parse(event.data)
-				if (payload.command === "start_c_debug") {
-					startDebugger(WORKSPACE_FOLDER, LAUNCH_CONFIG_C, payload.path)
-				}
-				if (payload.command === "start_python_debug") {
-					startDebugger(WORKSPACE_FOLDER, LAUNCH_CONFIG_PYTHON, payload.path)
-				}
+				startDebugger(WORKSPACE_FOLDER, payload)
 			});
 		}
 	});
