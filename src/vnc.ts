bf6a6dcc939e41fc638432ed0d084f287cf7a40c
Rongxin Liu <rongxinliu.dev@gmail.com>
2022-01-12 00:47:33 -0500

    open external uri when noVNC is ready

--- src/vnc.ts
+++ src/vnc.ts
@@ -1,8 +1,10 @@
 import * as vscode from 'vscode';
+import * as tcpPorts from 'tcp-port-used';
 import { exec } from 'child_process';
 
-export function startVNC() {
+export async function startVNC() {
     const vncPort = 6081;
+    const vncServerPort = 5900;
     const vncPassword = Math.random().toString(36).slice(-8);
     const vncHost = `${process.env.CODESPACE_NAME}-${vncPort}.githubpreview.dev`;
     const githubPreviewLink = `https://${vncHost}`;
@@ -37,14 +39,30 @@ export function startVNC() {
     });
 
     console.log("Starting noVNC client...");
-    exec(`/opt/noVNC/utils/novnc_proxy --vnc localhost:5900 --listen localhost:${vncPort}`, {
+    exec(`/opt/noVNC/utils/novnc_proxy --vnc localhost:${vncServerPort} --listen localhost:${vncPort}`, {
         "env": process.env
     }, (error, stdout, stderr) => {
         console.log(error, stdout, stderr);
     });
 
     // Open noVNC
-    setTimeout(() => {
-        vscode.env.openExternal(vscode.Uri.parse(vncUrl));
-    }, 1000);
+    let retries = 5;
+    while (retries > 0) {
+        await new Promise(r => setTimeout(r, 1000));
+        const didStartVncServer = await tcpPorts.check(vncServerPort);
+        const didStartVncClient = await tcpPorts.check(vncPort);
+        if (didStartVncServer && didStartVncClient) {
+            vscode.env.openExternal(vscode.Uri.parse(vncUrl));
+            break;
+        }
+        retries--;
+        if (retries == 0) {
+            if (!didStartVncServer) {
+                vscode.window.showInformationMessage(`Timed out before x11vnc was listening on port ${vncServerPort}`);
+            }
+            if (!didStartVncClient) {
+                vscode.window.showInformationMessage(`Timed out before noVNC was listening on port ${vncPort}`);
+            }
+        }
+    }
 }
