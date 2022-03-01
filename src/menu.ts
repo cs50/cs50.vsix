4ae4aa91b8633322e8e67ced2fd2809a12e8b989
Rongxin Liu <rongxinliu.dev@gmail.com>
2022-03-01 00:04:04 -0500

    used vscode styles for menu buttons

--- src/menu.ts
+++ src/menu.ts
@@ -21,13 +21,13 @@ class CS50ViewProvider implements vscode.WebviewViewProvider {
         panel.webview.onDidReceiveMessage(
             message => {
                 switch (message.command) {
-                    case "gui":
+                    case "launch_gui":
                         startVNC();
                         return;
-                    case "rebuild":
+                    case "rebuild_codespace":
                         vscode.commands.executeCommand("github.codespaces.rebuildEnvironment");
                         return;
-                    case "update":
+                    case "update_codespace":
                         if (vscode.window.activeTerminal == undefined) {
                             vscode.window.createTerminal();
                             vscode.commands.executeCommand("workbench.action.terminal.toggleTerminal");
@@ -43,35 +43,39 @@ class CS50ViewProvider implements vscode.WebviewViewProvider {
         );
     }
 
-    private _getHtmlForWebview(panel: vscode.Webview) {
+    private _getHtmlForWebview(webView: vscode.Webview) {
+        const styleVSCodeUri = webView.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'static', 'vscode.css'));
+		const styleMainUri = webView.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'static', 'main.css'));
         return `
 			<!DOCTYPE html>
 			<html lang="en">
 				<head>
 					<meta charset="UTF-8">
 					<meta name="viewport" content="width=device-width, initial-scale=1.0">
-					<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-					<title>CS50</title>
+                    <link href="${styleVSCodeUri}" rel="stylesheet">
 				</head>
 
 				<body class="bg-transparent">
 					<div class="btn-group-vertical">
-                        <button id="btn-gui" type="button" class="h5 btn btn-primary">GUI</button>
-						<button id="btn-update" type="button" class="h5 btn btn-success">Update Codespace</button>
-						<button id="btn-rebuild" type="button" class="h5 btn btn-danger">Rebuild Codespace</button>
+                        <br/>
+                        <button id="btn-gui" type="button" class="menu-button">Launch GUI</button>
+                        <br/>
+						<button id="btn-update" type="button" class="menu-button">Update Codespace</button>
+						<br/>
+                        <button id="btn-rebuild" type="button" class="menu-button">Rebuild Codespace</button>
 					</div>
 				</body>
 
 				<script>
                     const vscode = acquireVsCodeApi();
                     document.getElementById("btn-gui").onclick = () => {
-                        vscode.postMessage({command: "gui"})
+                        vscode.postMessage({command: "launch_gui"})
                     }
                     document.getElementById("btn-update").onclick = () => {
-                        vscode.postMessage({command: "update"})
+                        vscode.postMessage({command: "update_codespace"})
                     }
                     document.getElementById("btn-rebuild").onclick = () => {
-                        vscode.postMessage({command: "rebuild"})
+                        vscode.postMessage({command: "rebuild_codespace"})
                     }
 			    </script>
 			</html>
