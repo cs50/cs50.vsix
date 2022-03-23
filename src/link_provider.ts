9745d75ff9acaab2a29bcc6bb536decca9c61145
Rongxin Liu <rongxinliu.dev@gmail.com>
2022-03-23 18:59:24 -0400

    fixed indexing bug in link provider

--- src/link_provider.ts
+++ src/link_provider.ts
@@ -1,32 +1,40 @@
 import * as vscode from 'vscode';
 
+const LOCAL_HOST = "http://127.0.0.1";
+
 function openPreviewLinkAsLocalhostUrl() {
 
     // https://code.visualstudio.com/updates/v1_49#_terminal-link-providers
     vscode.window.registerTerminalLinkProvider({
+
         provideTerminalLinks: (context, token) => {
+          try {
 
-          // Detect the GitHub preview link if it exists and linkify it
-          const protocol = "https://"
-          const codespaceName = process.env.CODESPACE_NAME;
-          const githubPreviewDomain = "githubpreview.dev";
-          const startIndex = (context.line as string).indexOf(codespaceName) - protocol.length;
-          const endIndex = (context.line as string).indexOf(githubPreviewDomain) + githubPreviewDomain.length;
+            // Detect the GitHub preview link if it exists and linkify it
+            const protocol = "https://"
+            const codespaceName = process.env.CODESPACE_NAME;
+            const githubPreviewDomain = "githubpreview.dev";
+            const startIndex = (context.line as string).indexOf(codespaceName) - protocol.length;
+            const endIndex = (context.line as string).indexOf(githubPreviewDomain) + githubPreviewDomain.length;
 
-          // Extract port number
-          const port = (context.line as string).substring(startIndex + protocol.length + codespaceName.length + 1, endIndex - githubPreviewDomain.length - 1)
-          const localhost = "http://127.0.0.1"
-          if (startIndex === -1) {
+            // Extract port number
+            const port = (context.line as string).substring(startIndex + protocol.length + codespaceName.length + 1, endIndex - githubPreviewDomain.length - 1)
+
+            if (startIndex > -1) {
+              return [
+                {
+                  startIndex: startIndex,
+                  length: endIndex - startIndex,
+                  tooltip: 'Follow link',
+                  data: `${LOCAL_HOST}:${port}`
+                }
+              ];
+            }
+
+            return [];
+          } catch (error) {
             return [];
           }
-          return [
-            {
-              startIndex,
-              length: endIndex - startIndex,
-              tooltip: 'Follow link',
-              data: `${localhost}:${port}`
-            }
-          ];
         },
         handleTerminalLink: async (link: any) => {
             const uri = await vscode.env.asExternalUri(
