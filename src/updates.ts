8c72b8577c5f0fa69739522c6f77f0f3cc4123ad
Rongxin Liu <rongxinliu.dev@gmail.com>
2022-08-25 14:24:46 -0400

    use latest ref tag for updates, remove delayed updates

--- src/updates.ts
+++ src/updates.ts
@@ -6,32 +6,32 @@ const axios = require('axios').default;
 
 function checkForUpdates() {
     exec(`tail -1 /etc/issue`, (error, stdout, stderr) => {
-        const issue = stdout.trim();
-        const url = 'https://api.github.com/repos/cs50/codespace/commits/main';
+        const currentVersion = stdout.trim();
+        const url = 'https://api.github.com/repos/cs50/codespace/git/matching-refs/tags/latest';
         const headers = {
             'Authorization': `token ${process.env['GITHUB_TOKEN']}`,
             'Accept': 'application/vnd.github.v3+json'
         };
         axios.get(url, {headers: headers}).then((response) => {
-            const latest = response.data['sha'].trim();
-            const commit_time = new Date(response.data['commit']['committer']['date']).getTime();
-            const delta = (Date.now() - commit_time) / 1000;
-
-            // Wait 20 minutes to ensure the image is built
-            if (issue != latest && delta > 1200) {
-                const message = `Updates Available`;
-                vscode.window.showInformationMessage(
-                    message, ...['Update Now', 'Remind Me Later']).then((selection) => {
-                    if (selection === 'Update Now') {
-                        exec('/opt/cs50/bin/update50', (stdin, stdout, stderr) => {
-                            console.log(stderr);
-                        });
-                    }
-                });
+            const latestVersion = response.data[0]['object']['sha'].trim();
+            if (currentVersion != latestVersion) {
+                promptUpdate();
             }
         }).catch((error) => {
             console.log(error);
         });
+    });
+}
+
+function promptUpdate() {
+    const message = `Updates Available`;
+    vscode.window.showInformationMessage(
+        message, ...['Update Now', 'Remind Me Later']).then((selection) => {
+        if (selection === 'Update Now') {
+            exec('/opt/cs50/bin/update50', (stdin, stdout, stderr) => {
+                console.log(stderr);
+            });
+        }
     });
 }
 
