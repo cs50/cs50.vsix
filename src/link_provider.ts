0c671571289b3c24bfd9999c6c40d99c0e6fd0f1
Rongxin Liu <rongxinliu.dev@gmail.com>
2022-10-31 23:52:59 -0400

    show open preview url dialog

--- src/link_provider.ts
+++ src/link_provider.ts
@@ -1,3 +1,4 @@
+import { exec } from 'child_process';
 import * as vscode from 'vscode';
 
 const LOCAL_HOST = 'http://127.0.0.1';
@@ -8,33 +9,49 @@ function openPreviewLinkAsLocalhostUrl() {
     vscode.window.registerTerminalLinkProvider({
 
         provideTerminalLinks: (context, token) => {
-          try {
+            try {
 
-            // Detect the GitHub preview link if it exists and linkify it
-            const protocol = 'https://';
-            const codespaceName = process.env.CODESPACE_NAME;
-            const githubPreviewDomain = 'githubpreview.dev';
-            const startIndex = (context.line as string).indexOf(codespaceName) - protocol.length;
-            const endIndex = (context.line as string).indexOf(githubPreviewDomain) + githubPreviewDomain.length;
+                // Detect the GitHub preview link if it exists and linkify it
+                const protocol = 'https://';
+                const codespaceName = process.env.CODESPACE_NAME;
+                const githubPreviewDomain = 'githubpreview.dev';
+                const startIndex = (context.line as string).indexOf(codespaceName) - protocol.length;
+                const endIndex = (context.line as string).indexOf(githubPreviewDomain) + githubPreviewDomain.length;
 
-            // Extract port number
-            const port = (context.line as string).substring(startIndex + protocol.length + codespaceName.length + 1, endIndex - githubPreviewDomain.length - 1);
+                // Extract port number
+                const port = (context.line as string).substring(startIndex + protocol.length + codespaceName.length + 1, endIndex - githubPreviewDomain.length - 1);
 
-            if (startIndex > -1) {
-              return [
-                {
-                  startIndex: startIndex,
-                  length: endIndex - startIndex,
-                  tooltip: 'Follow link',
-                  data: `${LOCAL_HOST}:${port}`
+                if (startIndex > -1) {
+
+                    // GitHub Preview link
+                    const previewURL = `${protocol}${codespaceName}-${port}.${githubPreviewDomain}`;
+
+                    exec("pgrep flask || pgrep http-server", (error, stdout, stderr) => {
+                        if (error === null) {
+                            const message = `GitHub Preview URL: ${previewURL}`;
+                            vscode.window.showInformationMessage(
+                                message, ...['Open URL', 'Cancel']).then((selection) => {
+                                if (selection === 'Open URL') {
+                                    vscode.env.openExternal(vscode.Uri.parse(`${LOCAL_HOST}:${port}`));
+                                }
+                            });
+                        }
+                    });
+
+                    return [
+                        {
+                            startIndex: startIndex,
+                            length: endIndex - startIndex,
+                            tooltip: 'Follow link',
+                            data: `${LOCAL_HOST}:${port}`
+                        }
+                    ];
                 }
-              ];
-            }
 
-            return [];
-          } catch (error) {
-            return [];
-          }
+                return [];
+            } catch (error) {
+                return [];
+            }
         },
         handleTerminalLink: async (link: any) => {
             const uri = await vscode.env.asExternalUri(
